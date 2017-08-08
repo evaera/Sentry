5ba3a8a01c6ca6cdc9d0d5b090793e32a24eac86
Eryn Lynn <eryn@eryn.io>
2017-08-08 12:34:57 -0400

    trial mods cant remove

--- src/commands/sentry/ClearMutesCommand.js
+++ src/commands/sentry/ClearMutesCommand.js
@@ -20,6 +20,10 @@ class ClearMutesCommand extends Command {
 	}
 
 	async run(msg, args) {
+		if (msg.member.roles.has('218513797659230209')) { // trial moderator
+			return msg.reply("You don't have permission to use this command.");
+		}
+
 		let user = await Person.new(args.user.id);
 		if (!user) {
 			msg.reply("An error has occured");
--- src/commands/sentry/HistoryCommand.js
+++ src/commands/sentry/HistoryCommand.js
@@ -20,6 +20,10 @@ class HistoryCommand extends Command {
 	}
 
 	async run(msg, args) {
+		if (msg.member.roles.has('218513797659230209')) { // trial moderator
+			return msg.reply("You don't have permission to use this command. Please ask a Server Moderator to assist you.");
+		}
+
 		let histUser = await Person.new(args.user.id);
 		if (!histUser) {
 			msg.reply("An error has occured");
